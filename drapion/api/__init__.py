75e318a808c9bfbdecf9d41635c2712ced5a92e6
Giancarlo Rocha <giancarloiff@gmail.com>
2018-10-01 14:50:24 -0300

    Added api module

---
+++ drapion/api/__init__.py
@@ -0,0 +1,11 @@
+"""
+drapion.api
+~~~~~~~~~~~~~~~
+
+This module contains specific implementations for public API's
+
+:copyright: (c) 2018 Giancarlo Rocha
+:license: Apache 2.0, see LICENSE for more details.
+"""
+
+__all__ = []
