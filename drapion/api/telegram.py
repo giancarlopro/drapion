508b76deeb6ddd5e7d0cc143b8a1ca5332a42061
Giancarlo Rocha <giancarloiff@gmail.com>
2018-10-02 11:29:42 -0300

    Refactored TelegramBotAPI

--- drapion/api/telegram.py
+++ drapion/api/telegram.py
@@ -2,10 +2,14 @@ from drapion import Drapion, GenericObject
 
 
 class TelegramBotAPI(Drapion):
-    """Creates an Drapion instance from a telegram bot api token"""
+    """Creates an Drapion instance from a telegram bot api token
+    
+    See documentation for api reference:
+    https://core.telegram.org/bots/api
+    """
 
-    def __init__(self, token: str, parser = GenericObject):
+    def __init__(self, token: str):
         url = "https://api.telegram.org/bot{token}/".format(
             token=token
         )
-        super().__init__(url, parser)
+        super().__init__(url)
