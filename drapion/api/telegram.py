f252880c38cbaa4e5868f177587bfc5f38d059b8
Giancarlo Rocha <giancarloiff@gmail.com>
2018-10-03 13:43:30 -0300

    Added TelegramBotAPI custom parser

--- drapion/api/telegram.py
+++ drapion/api/telegram.py
@@ -1,5 +1,22 @@
-from drapion import Drapion
+import json
 
+from drapion import Drapion, parsers
+
+
+class TelegramParser(parsers.BaseParser):
+    @classmethod
+    def parse(cls, data):
+        """Validates and parse valid telegram api responses
+
+        :param data: JSON-serialized str
+        :return: Return from parsers.JSONParser.parse
+        """
+        obj = json.loads(data)
+
+        if obj['ok'] is False:
+            raise Exception(obj['description'])
+        
+        return parsers.JSONParser.parse(obj['result'])
 
 class TelegramBotAPI(Drapion):
     """Creates an Drapion instance from a telegram bot api token
@@ -12,4 +29,4 @@ class TelegramBotAPI(Drapion):
         url = "https://api.telegram.org/bot{token}/".format(
             token=token
         )
-        super().__init__(url)
+        super().__init__(url, parser=TelegramParser)
