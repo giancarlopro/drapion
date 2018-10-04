2cafadf03ca9dc7094bded98a95c1d2a59f1e918
Giancarlo Rocha <giancarloiff@gmail.com>
2018-10-04 12:07:19 -0300

    Added docstrings for parsers

--- drapion/parsers.py
+++ drapion/parsers.py
@@ -1,17 +1,32 @@
+"""
+drapion.parsers
+~~~~~~~~~~~~~~~
+
+This module implements Drapion core parsers
+
+:copyright: (c) 2018 Giancarlo Rocha
+:license: Apache 2.0, see LICENSE for more details.
+"""
+
 import json
-from typing import Dict, List
+from typing import Dict, List, Union
 
 from drapion import objects
 
 
 class BaseParser:
     @classmethod
-    def parse(cls, data):
+    def parse(cls, data: str) -> objects.DObject:
+        """Creates a DObject with a `content` attribute containing data
+
+        :param data: Data to put inside content attribute
+        :return: Instance of DObject
+        """
         return objects.DObject(attributes={'content': data})
 
 class JSONParser(BaseParser):
     @classmethod
-    def parse(cls, data):
+    def parse(cls, data: Union[str, Dict, List]) -> objects.DObject:
         """Parses a JSON into a DObject instance
 
         :param data: JSON str or parsed json e.g.: List or Dict
